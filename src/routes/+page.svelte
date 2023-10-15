62b1c537d2dc52bf1061b23feb4fbbe5d87fa055
naomifleurnoel <nperenboom@hotmail.com>
2023-10-15 23:05:37 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let firstName = 'Naomi';
+    let lastName = 'Perenboom';
+    let color = 'red';
+
+    let name = firstName + ' ' + lastName 
+
+    const toggle = () => {
+        color = color ==='red' ? 'pink' : 'red'
+    }
+</script>
+
+
+<main> 
+<h1 style= "color: {color}">Hi {name}, welcome! </h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={toggle}>Click me </button>
+</main>
